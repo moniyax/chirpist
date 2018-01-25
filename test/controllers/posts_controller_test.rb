26f1498bd568ea3f71c43088bb10451ff13a2ebf
Mayowa Yakub <mayoa.net@gmail.com>
2018-01-25 09:40:47 +0100

    Get tests to pass with devise auth

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,8 +1,11 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   setup do
     @post = posts(:one)
+    sign_in users(:one), scope: :user
   end
 
   test "should get index" do
