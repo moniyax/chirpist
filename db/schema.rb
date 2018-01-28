d80ffe55fd6371abd70543aff1dbd8bcdfaf0139
Mayowa Yakub <mayoa.net@gmail.com>
2018-01-28 21:51:11 +0100

    Set up devise confirmable module

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180128115234) do
+ActiveRecord::Schema.define(version: 20180128204659) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,11 @@ ActiveRecord::Schema.define(version: 20180128115234) do
     t.string   "moniker"
     t.string   "fullname"
     t.string   "description"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["moniker"], name: "index_users_on_moniker", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
