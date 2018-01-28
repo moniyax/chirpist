88013a79f077cca2b83f75d5e1956acd5f9498e8
Mayowa Yakub <mayoa.net@gmail.com>
2018-01-28 20:33:54 +0100

    Don't show notice banner for post creation success

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -30,7 +30,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to root_url, notice: 'Post was successfully created.' }
+        format.html { redirect_to root_url }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new }
